4f597d5dd97dbbca5a80bbc9348f0479652cbf37
hanung665 <hanung665@gmail.com>
2026-04-19 21:51:59 -0700

    Update wishas.js

--- src/js/wishas.js
+++ src/js/wishas.js
@@ -22,6 +22,7 @@ export const wishas = () => {
                 <img src=${data.icon} alt="bank icon animation">
                 <!-- /* <figcaption>No. Rekening {data.rekening.slice(0, 8)}xxxx <br> */ -->
                 <figcaption>${data.rekening} <br>${data.name}</figcaption>
+                
                 <button data-rekening=${data.rekening} aria-label="copy rekening">Salin No. Rekening</button>
            </figure>`
     );
