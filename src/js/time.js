ca75099b6dfbb06adf296346b5a8e446675b2088
hanung665 <hanung665@gmail.com>
2026-05-14 22:52:41 +0700

    Update time.js

--- src/js/time.js
+++ src/js/time.js
@@ -3,7 +3,7 @@ import { data } from "../assets/data/data.js";
 export const time = () => {
     const timeContainer = document.querySelector('.time');
     const [marriageDiv, receptionDiv] = timeContainer.querySelectorAll('div div');
-    const mapLink = timeContainer.querySelector('a');
+    const mapLink = timeContainer.querySelector('a+a');
     const addressParagraph = timeContainer.querySelector('a + p');
 
     const createTimeListItem = (title, details) => (
